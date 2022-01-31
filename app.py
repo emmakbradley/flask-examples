2931a645cb5b8425cf790ad0fda1f4413d58dd6b
emmakbradley <emmabradley@g.ucla.edu>
2022-01-31 00:23:17 -0800

    figured out how to display messages

--- app.py
+++ app.py
@@ -43,41 +43,82 @@ This set of lecture notes is based in part on previous materials developed by [E
 '''
 import sqlite3
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 app = Flask(__name__)
+
+def get_message_db():
+    if 'message_db' not in g:
+        g.message_db = sqlite3.connect("messages_db.sqlite")
+    g.message_db.execute('''CREATE TABLE IF NOT EXISTS messages(id INTEGER, handle TEXT, message TEXT)''')
+    return g.message_db
+def insert_message(request):
+    # connect to db
+    g.message_db = get_message_db()
+
+    # intialize cursor
+    cursor = g.message_db.cursor()
+    
+    # count rows
+    cursor.execute("select count(*) from messages")
+    rows = cursor.fetchone()[0]
+    
+    # get message data
+    message = request.form["message"]
+    handle = request.form["handle"]
+    this_id = 1 + rows
+    
+    # sql cmd
+    cmd = f"""INSERT INTO messages (id, handle, message)
+    VALUES('{this_id}', '{message}', '{handle}')"""
+    
+    # add message to db
+    cursor.execute(cmd)
+    g.message_db.commit()
+    
+    # close connection
+    g.message_db.close()
+    return
+
+def random_messages(n):
+    # connect to db
+    g.message_db = get_message_db()
+
+    # initialize cursor
+    cursor = g.message_db.cursor()
+
+    # grab n random messages
+    cmd = f"""SELECT * FROM messages ORDER BY RANDOM() LIMIT '{n}'"""
+    cursor.execute(cmd)
+
+    messages_list = cursor.fetchall()
+
+    # close database
+    g.message_db.close()
+
+    return messages_list
 
 @app.route("/")
 def main():
     return render_template('base.html')
     
-# getting basic user data
+# getting messages
 @app.route('/submit/', methods=['POST', 'GET'])
 def submit():
     if request.method == 'GET':
         return render_template('submit.html')
     else:
+        insert_message(request)
         try:
             return render_template('submit.html', thanks=True)
         except:
             return render_template('submit.hmtl', error=True)
-    #else:
-       # try:
-           # return render_template('ask.html', name=request.form['name'], student=request.form['student'])
-       # except:
-            #return render_template('ask.html')
 
 @app.route('/view/')
 def view():
-    return "not implemented yet"
+    messages_list = random_messages(3)
+    return render_template('view.html', m = messages_list)
     
-def get_message_db():
-    if g.message_db == True:
-        pass
-    else:
-        g.message_db = sqlite3.connect("messages_db.sqlite")
-    g.message_db.execute('''CREATE TABLE IF NOT EXISTS messages(id INTEGER, handle TEXT, message TEXT)''')
-    return g.message_db()
-    
-def insert_message():
 
-    return "done"
+    
+
+
